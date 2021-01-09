e3b2b38282230e5d225ee1cb2d9bd7a982dcb43b
Gideon Tong <gideon@gideontong.com>
2021-01-09 15:37:33 -0800

    Upgrade pie chart

--- webapp/pages/index/__init__.py
+++ webapp/pages/index/__init__.py
@@ -8,6 +8,10 @@ def index():
     global premium
     context = {
         'is_index': True,
+        'commands': '32,567,841',
+        'sales': '$15,340',
+        'tickets': 42,
+        'online': '4,311',
         'premium': premium
     }
     return render_template('index.html', **context)
