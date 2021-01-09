1b178ec416bf500ef75b152fd8f8d8e6cd466ad3
Gideon Tong <gideon@gideontong.com>
2021-01-08 17:12:11 -0800

    Prepare templates

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    # commands, earnings, tickets
+    return render_template('index.html')
