c4a077fb0b4edc9790b6d46638d696f0f75e5070
Gideon Tong <gideon@gideontong.com>
2021-01-09 11:59:56 -0800

    Prepare app

--- app.py
+++ app.py
@@ -12,3 +12,27 @@ def index():
         'premium': premium
     }
     return render_template('index.html', **context)
+
+@app.route('/statistics')
+def statistics():
+    pass
+
+@app.route('/settings')
+def settings():
+    pass
+
+@app.route('/shards')
+def shards():
+    pass
+
+@app.route('/guilds')
+def guilds():
+    pass
+
+@app.route('/channels')
+def channels():
+    pass
+
+@app.route('/users')
+def users():
+    pass
