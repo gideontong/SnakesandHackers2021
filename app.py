94ff81cefca451275fdcb736827f9c8a4ad11a85
Gideon Tong <gideon@gideontong.com>
2021-01-09 12:02:33 -0800

    Add context for pages

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from markupsafe import escape
 
 app = Flask(__name__)
 premium = True
@@ -15,22 +14,42 @@ def index():
 
 @app.route('/statistics')
 def statistics():
+    context = {
+        'is_stats': True,
+        'premium': premium
+    }
     pass
 
 @app.route('/settings')
 def settings():
+    context = {
+        'is_settings': True,
+        'premium': premium
+    }
     pass
 
 @app.route('/shards')
 def shards():
+    context = {
+        'is_shards': True,
+        'premium': premium
+    }
     pass
 
 @app.route('/guilds')
 def guilds():
+    context = {
+        'is_guilds': True,
+        'premium': premium
+    }
     pass
 
 @app.route('/channels')
 def channels():
+    context = {
+        'is_channels': True,
+        'premium': premium
+    }
     pass
 
 @app.route('/users')
